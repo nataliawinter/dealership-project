4dfb16396c4ee3cdaec76b70c8935ea54183fe2f
Natália Johann Winter <nataliawinter@gmail.com>
2018-10-03 15:00:01 -0300

    adjust words accents and psr (#2)

--- application/controllers/FaturamentoController.php
+++ application/controllers/FaturamentoController.php
@@ -2,28 +2,16 @@
 
 class FaturamentoController extends Zend_Controller_Action
 {
-
 	public function init(){
 
 	}
 
-	function faturamentoAction(){
-
+	function faturamentoAction()
+	{
 		$Veiculo = new Application_Model_DbTable_Veiculo();
 		$this->view->totalVeiculo = $Veiculo->totalVeiculosGanho();
 		
 		$totalVendas = new Application_Model_DbTable_Venda();
 		$this->view->totalVendasFeitas = $totalVendas->totalVenda();
-
 	}
-
-
 }
-
-
-
-
-
-
-
-
--- application/controllers/GastosveiculoController.php
+++ application/controllers/GastosveiculoController.php
@@ -2,22 +2,19 @@
 
 class GastosveiculoController extends Zend_Controller_Action
 {
-
-	public function init(){
+	public function init()
+	{
 
 	}
 
-	function gastosveiculoAction(){
-
+	function gastosveiculoAction()
+	{
 		$gastosveiculo = new Application_Model_DbTable_Gastosveiculo();
 		$this->view->gastosveiculo = $gastosveiculo->fetchAll();
-
-
 	}
 
-	//adiciona no album
-	function addgastosveiculoAction(){
-
+	function addgastosveiculoAction()
+	{
 		$form = new Application_Form_Gastosveiculo();
 		$form->submit->setLabel('salvar');
 		$this->view->form = $form;
@@ -25,7 +22,6 @@ class GastosveiculoController extends Zend_Controller_Action
 		if ($this->getRequest()->isPost()) {
 			$formData = $this->getRequest()->getPost();
 			if ($form->isValid($formData)) {
-				
 				$descricao = $form->getValue('Descricao');
 				$totalgasto = $form->getValue('TotalGasto');
 				$pago = $form->getValue('Pago');
@@ -33,21 +29,19 @@ class GastosveiculoController extends Zend_Controller_Action
 				
 				$guardaDataGasto = explode('/', $form->getValue('DataGasto'));
 				$datagasto = $guardaDataGasto[2] . '-' . $guardaDataGasto[1] . '-' . $guardaDataGasto[0];
-//				$datagasto = $form->getValue('DataGasto');
 				
 				$gastosveiculo = new Application_Model_DbTable_Gastosveiculo();
 				$gastosveiculo->addGastosveiculo($descricao, $totalgasto, $pago, $veiculo_idVeiculo, $datagasto);
 
 				$this->_helper->redirector('gastosveiculo');
-
 			} else {
 				$form->populate($formData);
 			}
 		}
-
 	}
 
-	function editgastosveiculoAction(){
+	function editgastosveiculoAction()
+	{
 		$form = new Application_Form_Gastosveiculo();
 		$form->submit->setLabel('salvar');
 		$this->view->form = $form;
@@ -55,9 +49,7 @@ class GastosveiculoController extends Zend_Controller_Action
 		if ($this->getRequest()->isPost()) {
 			$formData = $this->getRequest()->getPost();
 			if ($form->isValid($formData)) {
-
 				$id = (int)$form->getValue('idGastosveiculo');
-
 				$descricao = $form->getValue('Descricao');
 				$totalgasto = $form->getValue('TotalGasto');
 				$pago = $form->getValue('Pago');
@@ -68,11 +60,9 @@ class GastosveiculoController extends Zend_Controller_Action
 				$gastosveiculo->updateGastosveiculo($id, $descricao, $totalgasto, $pago, $veiculo_idVeiculo, $datagasto);
 				
 				$this->_helper->redirector('gastosveiculo');
-					
 			} else {
 				$form->populate($formData);
 			}
-
 		} else {
 			$id = $this->_getParam('idGastosveiculo', 0);
 			if ($id > 0) {
@@ -82,7 +72,8 @@ class GastosveiculoController extends Zend_Controller_Action
 		}
 	}
 
-	public function deletegastosveiculoAction(){
+	public function deletegastosveiculoAction()
+	{
 		if ($this->getRequest()->isPost()) {
 			$del = $this->getRequest()->getPost('del');
 			if ($del == 'Yes') {
@@ -91,25 +82,10 @@ class GastosveiculoController extends Zend_Controller_Action
 				$gastosveiculo->deleteGastosveiculo($id);
 			}
 			$this->_helper->redirector('gastosveiculo');
-
 		} else {
 			$id = $this->_getParam('idGastosveiculo', 0);
 			$gastosveiculo = new Application_Model_DbTable_Gastosveiculo();
 			$this->view->gastosveiculo = $gastosveiculo->getGastosveiculo($id);
 		}
 	}
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
